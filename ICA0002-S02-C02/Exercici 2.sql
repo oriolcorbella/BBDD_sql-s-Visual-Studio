d77eea1af3ee9c3d9fa4ffda4f226fc6f8001df7
oriol <uriolcorbella06@gmail.com>
2024-12-10 13:07:39 +0100

    save

---
+++ ICA0002-S02-C02/Exercici 2.sql
@@ -0,0 +1,35 @@
+a. Selecciona el nombre y apellido de los árbitros que sean jugadores
+    SELECT P.Nom, P.Cognom1 FROM Persona P WHERE P.Num_SS IN (
+    SELECT A.Num_SS_Arbitre
+    FROM Arbitre A
+    JOIN Jugador J ON A.Num_SS_Arbitre = J.Num_SS_Jugador
+);
+
+b. Selecciona el nombre y apellido de los árbitros que nop sean jugadores
+
+    SELECT P.Nom, P.Cognom1 FROM Persona P WHERE P.Num_SS IN (
+    SELECT A.Num_SS_Arbitre
+    FROM Arbitre A
+    MINUS
+    SELECT J.Num_SS_Jugador
+    FROM Jugador J
+);
+
+c. Muestra el número de partidos jugados por equipo como lucal de los 3 equipos que hayan jugado más
+
+    SELECT Equip_Local, COUNT(*) AS Num_Partidos FROM Partit GROUP BY Equip_Local ORDER BY Num_Partidos DESC LIMIT 3;
+
+d. Muestra la suma de presupuestos de todos los equipos
+
+    SELECT SUM(Pressupost) AS Suma_Presupuestos FROM Equip;
+
+e. Muestra el nombre y apellido de los jugadores ordenados por el apellido de forma ascendente y por el nombre de forma descendente
+
+    SELECT P.Nom, P.Cognom1 FROM Persona P JOIN Jugador J ON P.Num_SS = J.Num_SS_Jugador ORDER BY P.Cognom1 ASC, P.Nom DESC;
+
+f. Muestra equipos listados de la A a la Z filtrando solo los 3 primeros resultados
+
+    SELECT Nom_Equip FROM Equip ORDER BY Nom_Equip ASC LIMIT 3;
+
+
+
