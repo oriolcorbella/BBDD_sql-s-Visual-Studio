4313f607bc764c3eee11ebb20a0ac385c2f4390b
oriol <uriolcorbella06@gmail.com>
2025-01-17 09:55:29 +0100

    asf

---
+++ ICA0002-S03-C0's/ICA0002-S03-C02.sql
@@ -0,0 +1,10 @@
+
+UPDATE Federacion SET fecha_creacion = CURDATE() WHERE nombre_federacion = "FCF";
+DELETE FROM Competicion SET nombre_TV = null WHERE nombre_competicion = 'Champions';
+DELETE FROM Clasificacion;
+
+UPDATE Jugador SET Dorsal=1 WHERE Ubicacion="Portero"
+
+Update me cago en tu i no en suli
+
+
